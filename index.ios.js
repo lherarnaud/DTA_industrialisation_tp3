43c23d92c451714377f47ce174abedeb85450bdd
Arnaud L'her <arnaud.lher@laposte.net>
2017-07-20 11:36:41 +0200

    Corrections compatibilité ESLint

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Sentry } from 'react-native-sentry';
+
 
 import NavigationContainer from './src/NavigationContainer';
 
-import { Sentry } from 'react-native-sentry';
-
-Sentry.config("https://862af0d2191f496faf1f2f1b5b22d80b:98dff8a67b404635930478b15047d0b2@sentry.io/192724").install();
+Sentry.config('https://862af0d2191f496faf1f2f1b5b22d80b:98dff8a67b404635930478b15047d0b2@sentry.io/192724').install();
 
 
 export default function ReactNativeSample() {
